fb2f48c6a88a59cb6aff75b7057f0121e49cc76d
mihirmanju <60945712+mihirmanju@users.noreply.github.com>
2020-04-09 19:59:13 -0500

    1st commit

---
+++ myscripts.js
@@ -0,0 +1,8 @@
+
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "green";
+ctx.fillRect(20, 20, 25, 20);
+ctx.fillStyle = "red";
+ctx.fillRect(50, 200, 25, 20);
